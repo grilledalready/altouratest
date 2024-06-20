1c69749b12778dbabeeef85e601e1b5aae403ca4
N Sreedhar Babu <burnoutsree3@gmail.com>
2024-06-20 22:48:04 +0530

    Added some comments

--- Assets/Scripts/LoginData.cs
+++ Assets/Scripts/LoginData.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Keeping login data in class becuase in future if API calls are used the response will be stored in a class so simulating a similar scenario
 public static class LoginData
 {
     static string username = "admin";
